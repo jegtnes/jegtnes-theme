798cf2a88b6ef7b91bdedee94670bde91ac4bd13
Alex Jegtnes <saccosekk@gmail.com>
2014-05-01 18:36:13 +0100

    Adding LiveReload and Changed, replaced Ruby Sass with Node

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var changed = require('gulp-changed');
 
 var paths = {
   scripts: 'js/**/*.js',
@@ -20,6 +22,7 @@ gulp.task('scripts', function() {
 
 gulp.task('styles', function() {
     return gulp.src(paths.styles)
+      .pipe(changed('assets/css'))
       .pipe(sass({
         sourcemap: true,
         style: 'compressed'
@@ -39,9 +42,15 @@ gulp.task('images', function() {
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
+  var server = livereload();
+
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.styles, ['styles']);
   gulp.watch(paths.images, ['images']);
+
+  gulp.watch('assets/**').on('change', function(file) {
+      server.changed(file.path);
+  });
 });
 
 // The default task (called when you run `gulp` from cli)
