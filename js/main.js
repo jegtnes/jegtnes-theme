c94e458f655e470a365ede2c4b6378eb082cf075
Alex Jegtnes <saccosekk@gmail.com>
2014-05-13 16:21:39 +0100

    Comment stuff on document ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-document.getElementById("load-comments").addEventListener("click",function(e) {
-  this.parentNode.removeChild(this);
-	load_disqus();
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("load-comments").addEventListener("click",function(e) {
+    this.parentNode.removeChild(this);
+    load_disqus();
+  });
 });
